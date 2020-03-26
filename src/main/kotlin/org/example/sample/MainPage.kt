8d864ec5e274e833928f412500cebffdcff5b261
taylor <taylor.gagne1@ge.com>
2020-03-26 16:24:00 -0400

    First Commit

---
+++ src/main/kotlin/org/example/sample/MainPage.kt
@@ -0,0 +1,5 @@
+package org.example.sample
+
+class MainPage {
+
+}
---
+++ src/test/kotlin/org/example/sample/MainPageTest.kt
@@ -0,0 +1,10 @@
+package org.example.sample
+
+import org.junit.jupiter.api.Test
+
+class MainPageTest {
+    @Test
+    fun openPage() {
+
+    }
+}
